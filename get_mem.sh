8be77d41afd9132de2946f9148d838dca60d1e05
xxaxxelxx <x@axxel.net>
2019-08-09 12:33:08 +0200

    Updated 2019-08-09 12:33

--- get_mem.sh
+++ get_mem.sh
@@ -4,11 +4,11 @@
 PROCDIR="$1"
 test -r "$PROCDIR" || PROCDIR="/proc"
 PROCMEM="$PROCDIR/meminfo"
-MEMAVAILMINLIMIT="1000000"
+MEMMINLIMIT="1000000"
 
 #cat $PROCMEM
 test -r "$PROCMEM" || exit 2
-MEMAVAIL="$(cat $PROCMEM | grep 'MemAvailable' | awk '{print $2}')"
+MEMAVAIL="$(cat $PROCMEM | grep 'Inactive:' | awk '{print $2}')"
 
-test $MEMAVAIL -lt $MEMAVAILMINLIMIT && exit 1
+test $MEMAVAIL -lt $MEMMINLIMIT && exit 1
 exit 0
